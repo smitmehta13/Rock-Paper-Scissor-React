c298b51ac604b6aa73821993058cd4222bf4cc34
Smit Mehta <smitmehta.work@gmail.com>
2024-02-20 22:08:23 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const choices = ["ROCK", "PAPER", "SCISSOR"]
+
+const App = () => {
+
+const [computerChoice,setComputerChoice] = useState(null);
+const [playerChoice, setPlayerChoice] = useState(null)
+const [outCome, setOutCome] = useState(null)
+
+function computerAction(buttonSelected) {
+  const randomChoice = choices[Math.floor(Math.random()*choices.length)]
+  setComputerChoice(randomChoice)
+  setPlayerChoice(buttonSelected)
+  console.log("Computer's Choice " , computerChoice);
+  console.log("You Selected ", buttonSelected);
+  determineWinner(buttonSelected, randomChoice)
 }
+
+const determineWinner = (playerChoice, computerChoice) => {
+  if (playerChoice === computerChoice) {
+    setOutCome("Tie")
+  } else if (
+    (playerChoice === "ROCK" && computerChoice === "SCISSOR") ||
+    (playerChoice === "PAPER" && computerChoice === "ROCK") ||
+    (playerChoice === "SCISSOR" && computerChoice === "PAPER")
+  ) {
+    setOutCome("Player wins!");
+  } else {
+    setOutCome("Computer wins!");
+  }
+}
+
+
+return (
+  <div className="app-container">
+    <h1>Rock Paper Scissor</h1>
+
+    <div className="game-container">
+      <UserChoices onSelect={computerAction} />
+      <div className="col">
+        <h2>{computerChoice}</h2>
+      </div>
+    </div>
+
+    <div className="container">
+      <h2>{outCome}</h2>
+    </div>
+  </div>
+);
+}
+
+const UserChoices = ({ onSelect }) => (
+<div className="col">
+  {choices.map(choice => (
+    <button key={choice} onClick={() => onSelect(choice)}>
+      {choice}
+    </button>
+  ))}
+</div>
+);
 
 export default App;
